6ec4a646bbc833563cf9050d66cc32a3419ab2e3
Kayvan Vakili <kvakili813@gmail.com>
2018-03-04 18:41:33 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,26 @@
 def sort_array_asc(ascending)
-  ascending.asc 
+  ascending.sort
+end
+
+def sort_array_desc(descending)
+  descending.sort.reverse
+end
+
+def sort_array_char_count(strings)
+  strings.sort_by {|characters| characters.length}
+end
+
+def swap_elements(swaps)
+  swaps[1], swaps[2] = swaps[2], swaps[1]
+  swaps
+end
+
+def reverse_array(integers)
+  integers.reverse
+end
+
+def kesha_maker(strings)
+  strings.each do |kesha|
+    kesha.gsub("str[3]","$")
+  end
 end
